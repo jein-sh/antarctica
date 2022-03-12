06a6dc32d98cdb2769837edd75c48fa8ccc43a36
jein-sh <jein.sh85@gmail.com>
2022-03-12 20:18:40 +0200

    map.js

--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -18,3 +18,24 @@ ymaps.ready(function () {
 
 });
 
+
+
+
+// function init (ymaps) {
+//   let myMap = new ymaps.Map('map', {
+//     center: [59.93889, 30.32304],
+//     zoom: 16,
+//     }, {
+//       searchControlProvider: 'yandex#search'
+//   }),
+
+//   myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+
+//     iconLayout: 'default#image',
+//     iconImageHref: '../img/svg/pin.svg',
+//     iconImageSize: [18, 22],
+//     iconImageOffset: [0, -22],
+//   });
+
+//   myMap.geoObjects.add(myPlacemark);
+// }
