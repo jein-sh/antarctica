14d2b073cf9d0fe8cc5aa8e486223a94f7f6e702
jein-sh <jein.sh85@gmail.com>
2022-03-13 14:16:24 +0200

    Правки

--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -1,3 +1,5 @@
+let ymaps;
+
 ymaps.ready(function () {
   let myMap = new ymaps.Map('map', {
     center: [59.93889, 30.32304],
@@ -17,25 +19,3 @@ ymaps.ready(function () {
   myMap.geoObjects.add(myPlacemark);
 
 });
-
-
-
-
-// function init (ymaps) {
-//   let myMap = new ymaps.Map('map', {
-//     center: [59.93889, 30.32304],
-//     zoom: 16,
-//     }, {
-//       searchControlProvider: 'yandex#search'
-//   }),
-
-//   myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
-
-//     iconLayout: 'default#image',
-//     iconImageHref: '../img/svg/pin.svg',
-//     iconImageSize: [18, 22],
-//     iconImageOffset: [0, -22],
-//   });
-
-//   myMap.geoObjects.add(myPlacemark);
-// }
